e0a7db30979935ba2b4c7b6d39eaeecd36ed908b
Kristina Faye <kglinoga@gmail.com>
2022-06-26 18:10:07 -0700

    got the prompts and commands to happen, no relationship to the password generation yet though.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
 // Assignment Code
+var length;
+var availableChars = "";
+
+function generatePassword () {
+  var password = ""
+// prompt user for parameters
+// prompt for length, special chars, nmbers, uppercase, lowercase.
+getParameters();
+
+// 2nd half: create random password from available characters
+// maybe a for loop that runs the number of times in the length entry, adding a random character to the password string each time.
+
+for(i=0; i<length; i++){
+
+}
+
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +30,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getParameters() {
+  // set length via prompt
+  var length = prompt("Select password length with a number between 8 and 128") 
+    if (length || length >=8 || length <=129){
+      confirm("Tap OK if your password requires Special Characters. Tap Cancel if not.")
+     {
+      confirm("Tap OK if your password requires numbers. Tap Cancel if not.")
+    } {
+      confirm("Tap OK if your password requires Upper Case letters. Tap Cancel if not." )
+    } {
+      confirm("Tap OK if your password requires Lower Case letters. Tap Cancel if not.")
+    }
+  }
+  // add characters to availableChars via **confirm (not prompt)
+}
