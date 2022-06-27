33ba9916295ebadb175d75ab371b70dc439393c2
Kristina Faye <kglinoga@gmail.com>
2022-06-26 18:44:36 -0700

    OK! got them to require a number entry between 8 and 129, otherwise they get prompted to do so

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var length;
+var length //=user input from prompt;
 var availableUpperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var availableLowerCasechars = ["a", "b", "c", "d", "e", "f", "g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var availableSpecialChars = ["!", "@", "#", "$","%","^","&","*","(",")"]
@@ -11,11 +11,14 @@ function generatePassword () {
   // prompt user for parameters - OK yes, there are prompts for parameters, but we're not saving them anywhere?  maybe variables? 
 getParameters();
 
+
 // 2nd half: create random password from available characters
 // maybe a for loop that runs the number of times in the length entry, adding a random character to the password string each time.
 
 for(i=0; i<length; i++){
-  
+  return availableLowerCasechars
+  console.log(availableLowerCasechars)
+  return (availableLowerCasechars[Math.floor(Math.random()*availableLowerCasechars.length)])
 }
 
 }
@@ -36,8 +39,8 @@ generateBtn.addEventListener("click", writePassword);
 
 function getParameters() {
   // set length via prompt
-  var length = prompt("Select password length with a number between 8 and 128") 
-    if (length || length >=8 || length <=129){
+  var length = prompt("Select password length with a number between 8 and 128.") 
+    if (length >=8 && length <=129){
       confirm("Tap OK if your password requires Special Characters. Tap Cancel if not.");
      {
       confirm("Tap OK if your password requires numbers. Tap Cancel if not.");
@@ -46,7 +49,7 @@ function getParameters() {
     } {
       confirm("Tap OK if your password requires Lower Case letters. Tap Cancel if not.");
     }
-  } else if (length.type != number){
+  } else if (length.type != Number){
       prompt("Please choose a number between 8 and 129")
   }
   // add characters to availableChars via **confirm (not prompt)
